73b69f2c86309aa8eef5b315b8ce0187fad58ed3
Nate Dunn <hello@natedunn.net>
2025-03-07 21:02:31 -0600

    feat: add custom validation

--- lib/env/server.ts
+++ lib/env/server.ts
@@ -2,6 +2,8 @@ import { vercel } from '@t3-oss/env-core/presets-zod';
 import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
+import { logger } from '../../.kit/utils';
+
 export const env = createEnv({
 	extends: [vercel()],
 	server: {
@@ -11,7 +13,8 @@ export const env = createEnv({
 		GITHUB_CLIENT_SECRET: z.string(),
 		ADMIN_EMAIL: z.string(),
 		POLAR_ACCESS_TOKEN: z.string(),
-		NODE_ENV: z.enum(['development', 'production']),
+		POLAR_WEBHOOK_SECRET: z.string(),
+		NODE_ENV: z.enum(['development', 'production']).default('production'),
 	},
 	experimental__runtimeEnv: process.env,
 	isServer: typeof window === 'undefined',
