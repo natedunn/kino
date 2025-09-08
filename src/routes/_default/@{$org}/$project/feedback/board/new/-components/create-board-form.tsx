e704b54dc17d45f3727081b2566a301b81be82d9
Nate Dunn <hello@natedunn.net>
2025-09-08 10:55:48 -0600

    remove Authenticated component and add description

--- src/routes/_default/@{$org}/$project/feedback/board/new/-components/create-board-form.tsx
+++ src/routes/_default/@{$org}/$project/feedback/board/new/-components/create-board-form.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { useConvexMutation } from '@convex-dev/react-query';
 import { useForm } from '@tanstack/react-form';
 import { useMutation } from '@tanstack/react-query';
-import { Authenticated } from 'convex/react';
 import { ConvexError } from 'convex/values';
 
 import { api } from '~api';
@@ -12,6 +11,7 @@ import { InlineAlert } from '@/components/inline-alert';
 import { Label, LabelWrapper } from '@/components/label';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
 import { Id } from '@/convex/_generated/dataModel';
 import { feedbackBoardCreateSchema } from '@/convex/schema/feedbackBoard.schema';
 import { cn } from '@/lib/utils';
@@ -37,6 +37,7 @@ export const CreateBoardForm = ({ projectId }: { projectId: Id<'project'> }) => 
 	const defaultValues: FormSchema = {
 		projectId,
 		name: '',
+		description: '',
 	};
 
 	const form = useForm({
@@ -45,57 +46,76 @@ export const CreateBoardForm = ({ projectId }: { projectId: Id<'project'> }) => 
 			onChange: formSchema,
 		},
 		onSubmit: ({ value }) => {
+			setFormError(null);
 			createBoard(value);
 		},
 	});
 
 	return (
 		<div>
-			<Authenticated>
-				<form
-					onSubmit={(e) => {
-						e.preventDefault();
-						e.stopPropagation();
-						form.handleSubmit();
-					}}
-					className='flex flex-col gap-6'
-				>
-					<form.Field name='name'>
-						{(field) => {
-							return (
-								<div className='flex items-end gap-3'>
-									<div className='flex flex-1 flex-col gap-2'>
-										<LabelWrapper>
-											<Label>Name</Label>
-										</LabelWrapper>
-										<div className='flex items-center gap-4'>
-											<Input
-												value={field.state.value}
-												onChange={(e) => field.handleChange(e.target.value)}
-											/>
-										</div>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					e.stopPropagation();
+					form.handleSubmit();
+				}}
+				className='flex flex-col gap-6'
+			>
+				<form.Field name='name'>
+					{(field) => {
+						return (
+							<div className='flex items-end gap-3'>
+								<div className='flex flex-1 flex-col gap-2'>
+									<LabelWrapper>
+										<Label>Name</Label>
+									</LabelWrapper>
+									<div className='flex items-center gap-4'>
+										<Input
+											value={field.state.value}
+											onChange={(e) => field.handleChange(e.target.value)}
+										/>
 									</div>
 								</div>
-							);
-						}}
-					</form.Field>
+							</div>
+						);
+					}}
+				</form.Field>
 
-					{!!formError && <InlineAlert variant='danger'>{formError}</InlineAlert>}
+				<form.Field name='description'>
+					{(field) => {
+						return (
+							<div className='flex items-end gap-3'>
+								<div className='flex flex-1 flex-col gap-2'>
+									<LabelWrapper>
+										<Label>Description</Label>
+									</LabelWrapper>
+									<div className='flex items-center gap-4'>
+										<Textarea
+											value={field.state.value}
+											onChange={(e) => field.handleChange(e.target.value)}
+										/>
+									</div>
+								</div>
+							</div>
+						);
+					}}
+				</form.Field>
 
-					<div className='flex items-center gap-2'>
-						<form.Subscribe selector={(state) => [state.canSubmit, state.isSubmitting]}>
-							{([canSubmit, isSubmitting]) => (
-								<Button
-									type='submit'
-									className={cn({ 'opacity-50 grayscale select-none': !canSubmit })}
-								>
-									{isSubmitting ? 'Creating...' : 'Create'}
-								</Button>
-							)}
-						</form.Subscribe>
-					</div>
-				</form>
-			</Authenticated>
+				{!!formError && <InlineAlert variant='danger'>{formError}</InlineAlert>}
+
+				<div className='flex items-center gap-2'>
+					<form.Subscribe selector={(state) => [state.canSubmit, state.isSubmitting]}>
+						{([canSubmit, isSubmitting]) => (
+							<Button
+								type='submit'
+								className={cn({ 'opacity-50 grayscale select-none': !canSubmit })}
+							>
+								{isSubmitting ? 'Creating...' : 'Create'}
+							</Button>
+						)}
+					</form.Subscribe>
+				</div>
+			</form>
 		</div>
 	);
 };
