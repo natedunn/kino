879197a01ed9bd850ec8a22e668f1179d8d47898
Nate Dunn <hello@natedunn.net>
2025-09-16 23:12:58 -0600

    Small change to test deploy

--- src/routes/_marketing/index.tsx
+++ src/routes/_marketing/index.tsx
@@ -10,7 +10,7 @@ function RouteComponent() {
 			<div className='relative'>
 				<h1 className='relative z-10 text-gradient-primary text-8xl font-bold'>KINO</h1>
 				<h1 className='absolute top-10 scale-250 text-3xl font-bold opacity-100 blur-xl select-none'>
-					KINO
+					_KINO
 				</h1>
 			</div>
 		</div>
