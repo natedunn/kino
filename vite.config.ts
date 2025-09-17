fa0e8e8ed5cd6c08dcaf2972ea7463c07dd03998
Nate Dunn <hello@natedunn.net>
2025-09-16 20:40:02 -0600

    add vite cloudflare plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { cloudflare } from '@cloudflare/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
@@ -7,6 +8,7 @@ import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
 	plugins: [
+		cloudflare(),
 		ViteRestart({
 			// Due to hydration issues, we need to restart the server on changes to the following files
 			restart: ['./src/styles/**/*.css'],
