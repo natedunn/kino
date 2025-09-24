c330d9a164bfb9da1b3aa054950f433cc0e8d1de
Nate Dunn <hello@natedunn.net>
2025-09-24 12:36:43 -0600

    add cookies to fix unauthed issues on client

--- src/lib/auth/auth-server-utils.ts
+++ src/lib/auth/auth-server-utils.ts
@@ -1,7 +1,11 @@
 import { setupFetchClient } from '@convex-dev/better-auth/react-start';
+import { getCookie } from '@tanstack/react-start/server';
 
-import { createAuth } from '@/convex/api/auth';
+import { createAuth } from '@/convex/auth';
 
 // These helpers call Convex functions using a token from
 // Better Auth's cookies, if available.
-export const { fetchQuery, fetchMutation, fetchAction } = await setupFetchClient(createAuth);
+export const { fetchQuery, fetchMutation, fetchAction } = await setupFetchClient(
+	createAuth,
+	getCookie
+);
