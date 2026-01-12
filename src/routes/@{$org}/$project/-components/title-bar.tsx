3b60f554d65bff9b6d4c37aa7bcee6c310b05cdd
Nate Dunn <hello@natedunn.net>
2026-01-12 13:19:44 -0600

    fix type errors for now

--- src/routes/@{$org}/$project/-components/title-bar.tsx
+++
@@ -1,7 +0,0 @@
-type TitleBarProps = {
-	children: React.ReactNode;
-};
-
-export const TitleBar = ({ children }: TitleBarProps) => {
-	return <div className='m-4 px-4'>{children}</div>;
-};
---
+++ src/routes/@{$org}/$project/chat/route.tsx
@@ -0,0 +1,52 @@
+import { createFileRoute } from '@tanstack/react-router';
+
+export const Route = createFileRoute('/@{$org}/$project/chat')({
+	component: RouteComponent,
+});
+
+function RouteComponent() {
+	const { org, project } = Route.useParams();
+
+	return (
+		<div className='container h-full'>
+			{org} , {project}
+			{/* <SecondaryMenuLayout
+				title='Channels'
+				items={[
+					{
+						to: '/@{$org}/$project/chat/$chatId',
+						params: {
+							org,
+							project,
+							chatId: 'general',
+						},
+						children: 'General',
+						icon: '✨',
+					},
+					{
+						to: '/@{$org}/$project/chat/$chatId',
+						params: {
+							org,
+							project,
+							chatId: 'stack',
+						},
+						children: 'Stack',
+						icon: '⚙️',
+					},
+					{
+						to: '/@{$org}/$project/chat/$chatId',
+						params: {
+							org,
+							project,
+							chatId: 'off-topic',
+						},
+						children: 'Off-topic',
+						icon: '👀',
+					},
+				]}
+			>
+				<Outlet />
+			</SecondaryMenuLayout> */}
+		</div>
+	);
+}
