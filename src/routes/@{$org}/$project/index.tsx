87d93332c7aa5b42aa9ad292e16a669dcfaf0ba7
Nate Dunn <hello@natedunn.net>
2026-06-10 19:50:21 -0600

    Show empty project overview page

--- src/routes/@{$org}/$project/index.tsx
+++ src/routes/@{$org}/$project/index.tsx
@@ -1,10 +1,13 @@
-import { Navigate, createFileRoute } from '@tanstack/react-router';
+import { createFileRoute } from "@tanstack/react-router"
 
-export const Route = createFileRoute('/@{$org}/$project/')({
+export const Route = createFileRoute("/@{$org}/$project/")({
   component: ProjectIndexRoute,
-});
+})
 
 function ProjectIndexRoute() {
-  const params = Route.useParams();
-  return <Navigate params={params} to="/@{$org}/$project/feedback" />;
+  return (
+    <div className="container py-8">
+      <h1 className="text-2xl font-bold md:text-3xl">Overview</h1>
+    </div>
+  )
 }
