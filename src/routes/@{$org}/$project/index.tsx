a0d36de732cdba44f128e6d5a672fb26271fd436
Nate Dunn <hello@natedunn.net>
2026-06-10 19:52:21 -0600

    Add project settings link to overview

--- src/routes/@{$org}/$project/index.tsx
+++ src/routes/@{$org}/$project/index.tsx
@@ -1,13 +1,22 @@
-import { createFileRoute } from "@tanstack/react-router"
+import { Link, createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/@{$org}/$project/")({
   component: ProjectIndexRoute,
 })
 
 function ProjectIndexRoute() {
+  const params = Route.useParams()
+
   return (
     <div className="container py-8">
       <h1 className="text-2xl font-bold md:text-3xl">Overview</h1>
+      <Link
+        className="link-text mt-4 inline-flex text-sm"
+        params={params}
+        to="/@{$org}/$project/options"
+      >
+        Project settings
+      </Link>
     </div>
   )
 }
