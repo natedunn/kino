0b3994dbf392bbc5313aaef9f7b855a433ae399a
Nate Dunn <hello@natedunn.net>
2026-08-28 18:46:53 -0600

    fix auth schema test type cast

--- src/lib/account-schema.test.ts
+++ src/lib/account-schema.test.ts
@@ -4,7 +4,9 @@ import { accountTable } from '../../convex/functions/schema';
 
 	describe('account auth schema', () => {
 		it('defines the Better Auth compound identity index in issuer/accountId order', () => {
-			const indexes = (accountTable as { export(): { indexes: Array<unknown> } }).export().indexes;
+			const indexes = (
+				accountTable as unknown as { export(): { indexes: Array<unknown> } }
+			).export().indexes;
 			expect(indexes).toEqual(
 				expect.arrayContaining([
 					expect.objectContaining({
